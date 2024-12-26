64a95a93f0d9df2b427fc87838d1f2e8c38b4b06
Bright <bright.andohh@gmail.com>
2024-12-26 14:31:38 -0600

    schedule-builder-02

--- graphical/main.c
+++ graphical/main.c
@@ -6,14 +6,14 @@
 -- Inspired to have a little fun with the C language, I created this Schedule Builder. --
 -- It's a simple program that takes your start date and end date and helps you plan.   --
 -- Who says practicality can't be fun? Let's dive into some C magic!                   --
+-- ncurses adds stdio.h, unctrl.h, stdarg.h, stddef.h so we don't need to re-include   --
 ----------------------------------------------------------------------------------------
 */
 
 #include <ncurses.h>
 #include <stdlib.h>
+#include <time.h>
 
-#define WIDTH 10
-#define HEIGHT 20
 
 typedef struct _point{
     int x;
@@ -50,40 +50,62 @@ void freeDMY(DMY *dmy){
     free(dmy);
 }
 
+Point *createPoint(int x, int y){
+    Point *p1 = malloc(sizeof(Point));
+    p1->x = x;
+    p1->y = y;
+
+    return p1;
+}
+
+void freePoint(Point *point){
+    free(point);
+}
+
+void printDate(DMY *dmy){
+    printw("----------------------------------------------------------\n"
+           "|START DATE: <%d> - <%d> - <%d>                         |\n"
+           "----------------------------------------------------------\n", 
+           dmy->day, dmy->month, dmy->year);
+}
+
+void printDetails(){
+    printw(
+            "----------------------------------- SCHEDULE BUILDER -----------------------------------\n"
+            "-- Because who needs paper, right? It's not like it's been working for centuries.     --\n"
+            "-- Instead of jotting schedules on your phone like a normal person, I thought:        --\n"
+            "-- Hey, why not overcomplicate it and write a whole program in C?                     --\n"
+            "-- This masterpiece takes your start and end dates and does the heavy lifting.        --\n"
+            "-- It's totally not because I was bored or looking for an excuse to mess with C.      --\n"
+            "-- Anyway, enjoy this marvel of modern procrastination engineering.                   --\n"
+            "----------------------------------------------------------------------------------------\n"
+    );
+}
+
 int main(void){
 
-    initscr();
-    // if (has_colors() == FALSE) {  // Check if terminal supports color
-    //     endwin();
-    //     printf("Your terminal does not support color\n");
-    //     return 1;
-    // }
-
-    // start_color();
-    // init_pair(1, COLOR_RED, COLOR_BLACK);          //blue for text and black for background
-
-    // bkgd(COLOR_PAIR(1));  // Set the background of the entire screen
-    // clear();
+    initscr();                                       //initialize ncurses
+    time_t t = time(NULL);
+    struct tm *tm = localtime(&t);                  //to get current date
+    // Point *moveScreen = createPoint(5,2);
+    DMY *userStartDate = createStartDate(00,00,0000);
 
     int day,month,year;
-    // attron(COLOR_PAIR(1));
-    printw(
-    "----------------------------------- SCHEDULE BUILDER -----------------------------------\n"
-    "-- Because who needs paper, right? It's not like it's been working for centuries.     --\n"
-    "-- Instead of jotting schedules on your phone like a normal person, I thought:        --\n"
-    "-- Hey, why not overcomplicate it and write a whole program in C?                     --\n"
-    "-- This masterpiece takes your start and end dates and does the heavy lifting.        --\n"
-    "-- It's totally not because I was bored or looking for an excuse to mess with C.      --\n"
-    "-- Anyway, enjoy this marvel of modern procrastination engineering.                   --\n"
-    "----------------------------------------------------------------------------------------\n"
-    );
+    // move(moveScreen->y, moveScreen->x);
+    printDetails();
 
-    // attroff(COLOR_PAIR(1));
-    printw("Enter your start date: <dd-mm-yyy>");
-
+    printw("Today's date is %d-%d-%d on the system\n",tm->tm_mday,tm->tm_mon + 1, tm->tm_year + 1900);
+    addstr("Enter your start date: <dd-mm-yyy>: ");
+    scanw("%d %d %d", &userStartDate->day, &userStartDate->month, &userStartDate->year);
+    printDate(userStartDate);
+    //handle error if user input exceeds our expectations 
     refresh();
     getch();
+    
     endwin();
+
+    // freePoint(moveScreen);
+    freeDMY(userStartDate);
 
     return 0;
 }
